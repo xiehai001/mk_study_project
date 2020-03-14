6e22cb83bdac9e0f7d57c75ca53ebcda15038327
花裤熊二 <404437516@qq.com>
2020-03-14 12:00:05 +0800

    vuelidate 学习

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <div class="layui-container">
       <form class="layui-form layui-form-pane" action>
-        <div class="layui-form-item">
+        <div class="layui-form-item" :class="{'form-group-error':$v.name.$error}">
           <label class="layui-form-label">用户名</label>
-          <div class="layui-input-block">
+          <div class="layui-input-inline">
             <input
               type="text"
               name="title"
@@ -14,12 +14,15 @@
               autocomplete="off"
               class="layui-input"
               v-model.trim="name"
+              @input="setName($event.target.value)"
             />
           </div>
+          <div class="error" v-if="!$v.name.required">用户民不能空</div>
+          <div class="error" v-if="!$v.name.email">用户民输入格式有误</div>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">密码</label>
-          <div class="layui-input-block">
+          <div class="layui-input-inline">
             <input
               type="password"
               name="title"
@@ -31,6 +34,7 @@
               v-model.trim="password"
             />
           </div>
+            <div class="error" v-if="!$v.password.required">密码不能空</div>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">验证码</label>
@@ -47,8 +51,9 @@
             />
           </div>
           <div class="layui-form-mid svg" v-html="captchaImg" @click="changeCapcha"></div>
+          <div class="error" v-if="!$v.code.required">验证码不能为空</div>
         </div>
-        <button type="button" class="layui-btn">点击登录</button>
+        <button type="button" class="layui-btn" @click="checkFrom">点击登录</button>
         <a class="xh-link" href="">忘记密码</a>
       </form>
     </div>
@@ -56,6 +61,7 @@
 </template>
 <script>
 import axios from 'axios'
+import { required, email } from 'vuelidate/lib/validators'
 export default {
   name: 'app',
   data () {
@@ -63,7 +69,20 @@ export default {
       captchaImg: null,
       name: '',
       password: '',
-      code: ''
+      code: '',
+      errMsg: []
+    }
+  },
+  validations: {
+    name: {
+      required,
+      email
+    },
+    password: {
+      required
+    },
+    code: {
+      required
     }
   },
   mounted () {
@@ -88,6 +107,10 @@ export default {
       if (!this.code) {
         this.errMsg.push('验证码不能为空')
       }
+    },
+    setName (value) {
+      this.name = value
+      this.$v.name.$touch()
     }
   }
 }
@@ -117,5 +140,14 @@ export default {
   .svg {
     position: relative;
     top: -15px;
+  }
+  .error{
+    display: none;
+  }
+  .form-group-error{
+    .error{
+      display: block;
+      color: red;
+    }
   }
 </style>
