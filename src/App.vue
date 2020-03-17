daf866523fe717f766e4b136e8ad2412089bcf72
花裤熊二 <404437516@qq.com>
2020-03-17 11:12:59 +0800

    登录，注册，忘记密码页面 添加和页面间路由跳转添加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
+    <s-header></s-header>
     <router-view></router-view>
+    <s-footer></s-footer>
   </div>
 </template>
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    SHeader: Header,
+    SFooter: Footer
+  }
 }
 </script>
 <style lang="scss">
- @import "assets/layui/css/layui";
- @import "assets/css/global.css";
- @import "assets/layui/css/modules/layer/default/layer.css"
+@import "assets/layui/css/layui.css";
+@import "assets/css/global.css";
+@import "assets/layui/css/modules/layer/default/layer.css";
 </style>
